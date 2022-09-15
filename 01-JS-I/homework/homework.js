e043eb5a8e48ba669abb77c11becbc91be86fdc6
Itskariuwu <Karenelizarv@gmail.com>
2022-09-14 20:16:21 -0500

    Primera tarea lista ;)

--- 01-JS-I/homework/homework.js
+++ 01-JS-I/homework/homework.js
@@ -1,22 +1,22 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = null;
+const nuevaString = 'Hola Elizabeth';
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = 1109;
 
 // Crea una variable booleana:
-const nuevoBool = null;
+const nuevoBool = 8 === '8';
 
 // Resuelve el siguiente problema matemático:
-const nuevaResta = 10 - null === 5;
+const nuevaResta = 10 - null === 10;
 
 // Resuelve el siguiente problema matemático:
-const nuevaMultiplicacion = 10 * null === 40 ;
+const nuevaMultiplicacion = 10 * null === 0 ;
 
 // Resuelve el siguiente problema matemático:
-const nuevoModulo = 21 % 5 === null;
+const nuevoModulo = 21 % 5 === 1;
 
 // No modificar nada debajo de esta línea
 // --------------------------------
